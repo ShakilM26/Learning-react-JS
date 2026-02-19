4d5db69aaea983c31aa91a8bef2da4f66feafea8
Shakil <shakilkabir.h@gmail.com>
2026-02-19 18:49:19 +0600

    Add files via upload

--- counter/Counter.jsx
+++ counter/Counter.jsx
@@ -1,1 +1,16 @@
+import React, {useState} from 'react';
 
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>You've Clicked {count}</p>
+            <button onClick={() => setCount(count+1)}>Increase</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={() => count > 0 && setCount(count - 1)}>Decrease</button>
+        </div>
+    );
+}
+
+export default Counter;
