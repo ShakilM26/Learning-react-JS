651298a1509802883f1acd27b7c617a54202f63b
Shakil <shakilkabir.h@gmail.com>
2026-02-19 19:07:20 +0600

    Add files via upload

--- counter/Counter.jsx
+++ counter/Counter.jsx
@@ -2,12 +2,25 @@ import React, {useState} from 'react';
 
 function Counter() {
     const [count, setCount] = useState(0);
+    // count = state value
+    // setCount = number changing function
 
     return (
         <div>
-            <p>You've Clicked {count}</p>
+            <h1><strong>Counter App</strong></h1>
+
+            {/* taking values from state and displaying */}
+            <h3>Shakil Clicked: {count} times</h3>
+            <br></br>
+
+            {/* count = (present value) + 1 */}
             <button onClick={() => setCount(count+1)}>Increase</button>
+            
             <button onClick={() => setCount(0)}>Reset</button>
+            
+
+            {/* first check condition, if it's tru then  
+            "setCount(count - 1)" will run */}
             <button onClick={() => count > 0 && setCount(count - 1)}>Decrease</button>
         </div>
     );
