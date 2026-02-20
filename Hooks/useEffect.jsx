ab58c9e988eae705779b81aad09d9aa19d08a98e
Shakil <shakilkabir.h@gmail.com>
2026-02-20 23:57:44 +0600

    Add files via upload

--- Hooks/useEffect.jsx
+++ Hooks/useEffect.jsx
@@ -1,1 +1,21 @@
+import React, { useState, useEffect } from 'react'; 
 
+function ScoreTrack() {
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+
+    document.title = `Current Score: ${score}`;
+
+  }, [score]); // dependency array
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2>Live Football Match</h2>
+      <p>Team Eagle has scored: {score} goals</p>
+      <button onClick={() => setScore(score + 1)}>Increase Score</button>
+    </div>
+  );
+}
+
+export default ScoreTrack;
