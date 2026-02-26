07c6721c7b11695bccf458f15fbfc399b0b7a25c
Shakil <shakilkabir.h@gmail.com>
2026-02-26 23:57:10 +0600

    Add files via upload

--- Events/1. Basic_onClickEvent.jsx
+++ Events/1. Basic_onClickEvent.jsx
@@ -1,1 +1,17 @@
+// 1. Basic onClick Event
 
+export default function BasicEvent() {
+    function handleClick() {
+        alert("Button Clicked")
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>Click Me</button>
+        </div>
+    )
+}
+
+// onClick - react Event
+// {handleClick} -> function reference
+// when click function will execute
