67f84dee36c2dbf1b319a2994ac0ac9d0037ee66
Shakil <shakilkabir.h@gmail.com>
2026-03-08 11:06:45 +0600

    Add files via upload

--- HookForm/with Controller/HookForm.jsx
+++ HookForm/with Controller/HookForm.jsx
@@ -1,1 +1,52 @@
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {formSchema} from '../components/Schema'
 
+export default function Form(){
+    const{
+        control, register, handleSubmit, 
+        formState: {errors}
+    } = useForm({
+        resolver: zodResolver(formSchema)
+    });
+
+    const onSubmit = (data) => {
+        console.log("Form Data:", data);
+        alert('Succesfully Submited')
+    }
+
+    return(
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h2>Simple User Form</h2>
+
+            <div>
+                <input {...register('name')} placeholder="Name"/>
+                <p>{errors.name?.message}</p>
+            </div>
+
+            <div>
+                <input
+                type='number' 
+                {...register('age', {valueAsNumber: true})} placeholder="Age"/>
+                <p>{errors.age?.message}</p>
+            </div>
+
+            <div>
+                <input {...register('email')} placeholder="Email"/>
+                <p>{errors.email?.message}</p>
+            </div>
+
+            <div>
+                <Controller
+                    name='location'
+                    control={control}
+                    render={({field}) => (
+                        <input {...field} placeholder="Enter your location" />
+                    )}
+                />
+                <p>{errors.location?.message}</p>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
